16952639d8c37ebe4f53e402ddf559153f9400cf
S-Brand5136 <Brandon.Shemilt@live.co.uk>
2023-01-14 17:20:16 -0700

    Add new contants and comments

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,8 @@
+// poll-user constants
 export const VOTE_SENT = "VOTE_SENT";
 export const VOTE_PENDING = "VOTE_PENDING";
 
+// index page/poll-form constants
 export const VOTE_POSTED = "VOTE_POSTED";
-export const VOTE_EDITTING = "VOTE_EDITTING";
+export const VOTE_USER_VIEW = "VOTE_USER_VIEW";
+export const VOTE_FORM_VIEW = "VOTE_FORM_VIEW";
