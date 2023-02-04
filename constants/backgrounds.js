8848c07649db760c91a57bc467272344d1d52f48
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2023-02-04 13:44:29 -0700

    Remove particles and trianglify backgrounds

--- constants/backgrounds.js
+++ constants/backgrounds.js
@@ -1,21 +1,5 @@
 export const backgrounds = [
   {
-    title: "trianglify-1",
-    path: "/backgrounds/trianglify-1.png",
-  },
-  {
-    title: "trianglify-2",
-    path: "/backgrounds/trianglify-2.png",
-  },
-  {
-    title: "trianglify-3",
-    path: "/backgrounds/trianglify-3.png",
-  },
-  {
-    title: "trianglify-4",
-    path: "/backgrounds/trianglify-4.png",
-  },
-  {
     title: "topography-0",
     path: "/backgrounds/topography-0.svg",
   },
@@ -34,22 +18,6 @@ export const backgrounds = [
   {
     title: "topography-4",
     path: "/backgrounds/topography-4.svg",
-  },
-  {
-    title: "Particles-4",
-    path: "/backgrounds/Particles-4.png",
-  },
-  {
-    title: "Particles-5",
-    path: "/backgrounds/Particles-5.png",
-  },
-  {
-    title: "Particles-6",
-    path: "/backgrounds/Particles-6.png",
-  },
-  {
-    title: "Particles-7",
-    path: "/backgrounds/Particles-7.png",
   },
   {
     title: "gradient-8",
