2b8acde2ff959983a8d0b50fe0ee191436bb139f
S-Brand5136 <Brandon.Shemilt@live.co.uk>
2023-03-18 14:16:39 -0600

    Refactor: Only allow post methods

--- pages/api/poll.js
+++ pages/api/poll.js
@@ -1,81 +1,84 @@
 import prisma from "../../lib/prisma";
-import { compareDates } from "../../lib/compareDates";
 
 export default async function handler(req, res) {
-  /*
-    POST: a new poll to the db
-    BODY: {
-      title:          required | string
-      options:        required | array    [ question ]
-      colorScheme:    optional | string,  defaults to blue
-      hideVotes:      optional | boolean, defaults to false
-      startDate:      required | UTC date
-      endDate:      required | UTC date
-      backgroundURL:  optional | string,  defaults to empty
-    }
-    URL: posts/[id]
-  */
-  if (req.method === "POST") {
-    try {
-      const {
-        title,
-        options,
-        colorScheme = "blue",
-        hideVotes = false,
-        startDate,
-        endDate,
-        background = "",
-      } = req.body;
-
-      const startDay = new Date(startDate);
-      const endDay = new Date(endDate);
-
-      if (startDay > new Date()) {
-        throw new Error("Start date is invalid");
-      }
-
-      if (endDay < startDay) {
-        throw new Error("end date is invalid");
-      }
-
-      // create poll
-      const result = await prisma.poll.create({
-        data: {
+  switch (req.method) {
+    /*
+                POST: a new poll to the db
+                BODY: {
+                  title:          required | string
+                  options:        required | array    [ question ]
+                  colorScheme:    optional | string,  defaults to blue
+                  hideVotes:      optional | boolean, defaults to false
+                  startDate:      required | UTC date
+                  endDate:      required | UTC date
+                  backgroundURL:  optional | string,  defaults to empty
+                }
+                URL: posts/[id]
+              */
+    case "POST":
+      try {
+        const {
           title,
-          colorScheme,
-          hideVotes,
-          startDate: startDay.toISOString().toString(),
-          endDate: endDay.toISOString().toString(),
-          backgroundURL: background,
-        },
-      });
+          options,
+          colorScheme = "blue",
+          hideVotes = false,
+          startDate,
+          endDate,
+          background = "",
+        } = req.body;
 
-      if (!result) {
-        throw new Error("Could not create poll");
+        const startDay = new Date(startDate);
+        const endDay = new Date(endDate);
+
+        if (startDay > new Date()) {
+          throw new Error("Start date is invalid");
+        }
+
+        if (endDay < startDay) {
+          throw new Error("end date is invalid");
+        }
+
+        // create poll
+        const result = await prisma.poll.create({
+          data: {
+            title,
+            colorScheme,
+            hideVotes,
+            startDate: startDay.toISOString().toString(),
+            endDate: endDay.toISOString().toString(),
+            backgroundURL: background,
+          },
+        });
+
+        if (!result) {
+          throw new Error("Could not create poll");
+        }
+
+        // create questions for poll
+        const questions = await Promise.all(
+          options.map((option) => {
+            return prisma.question.create({
+              data: {
+                question: option.question,
+                votes: 0,
+                pollId: result.id,
+              },
+            });
+          })
+        );
+
+        // placeholder return value from inserting post into DB
+        res.status(200).json({
+          poll: result,
+          questions,
+        });
+      } catch (error) {
+        return res.status(500).json({
+          error,
+        });
       }
-
-      // create questions for poll
-      const questions = await Promise.all(
-        options.map((option) => {
-          return prisma.question.create({
-            data: {
-              question: option.question,
-              votes: 0,
-              pollId: result.id,
-            },
-          });
-        })
-      );
-
-      // placeholder return value from inserting post into DB
-      res.status(200).json({
-        poll: result,
-        questions,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        error,
-      });
-    }
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end("");
   }
 }
